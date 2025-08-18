593b575f3ccee1b6afe132aca7dde5c15dc6643f
Arlindo Boa <arlindojosboa@gmail.com>
2025-08-18 09:28:37 +0200

    Created A complete set up

--- erpnext_mz/modules/accounting/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Accounting module removed in clean skeleton.
-"""
-
-__all__: list[str] = []
--- erpnext_mz/modules/hr_payroll/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-HR & Payroll module removed in clean skeleton.
-"""
-
-__all__: list[str] = []
